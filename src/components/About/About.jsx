d702bf134161cbab3099943f05ee17cd7471b1a5
DLopez6877 <dlopez6877@gmail.com>
2024-10-08 00:40:35 -0700

    fix scrolling

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,24 +1,46 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useLayoutEffect, useRef } from 'react';
 import './About.scss'
 import aboutbg from '../../assets/aboutbg.png';
 import SplitText from '../SplitText/SplitText';
-import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import RoundMouse from '../../wrappers/RoundMouse/RoundMouse';
 
 const About = () => {
     const containerRef = useRef(null);
+    const bgImgRef = useRef(null);
+
+    useLayoutEffect(() => {
+        const container = containerRef.current;
+        const bgImg = bgImgRef.current;
+
+        ScrollTrigger.refresh();
+
+        const scrollTrigger = ScrollTrigger.create({
+            trigger: container,
+            start: 'top top',
+            end: () => `${container.offsetHeight - window.innerHeight}px`,
+            pin: bgImg,
+            pinSpacing: true,
+            scrub: 1,
+        });
+
+        return () => {
+            scrollTrigger.kill();
+        };
+
+    }, []);
 
     return (
         <RoundMouse>
             <div ref={containerRef} className="about-container">
-                <img className="about-bg-img" src={aboutbg} alt="Background of orange geometric trees with a blue sunrise." />
+                <img ref={bgImgRef} className="about-bg-img" src={aboutbg} alt="Background of orange geometric trees with a blue sunrise." />
                 <div className="about-headline">
                     <div className="glow headline-text"><span>ABOUT M<span className='e'>E</span></span></div>
                     <p className='seeking-opportunities small-text'>Currently seeking new opportunities.</p>
                 </div>
                 <div className="about-bio container">
                     <SplitText><p>I’ve spent over seven years as a full-stack software engineer, but my heart belongs to frontend development. After years of venturing deeper and deeper into the dark depths of backend work, it’s time to grab this Balrog, known as my career, by the horns and take control.</p></SplitText>
-                    <SplitText><p>I want to build Awwwards winning websites where I can let my creativity shine.</p></SplitText>
+                    <SplitText><p>I want to help build Awwwards winning websites where I can let my creativity shine.</p></SplitText>
                 </div>
             </div>
         </RoundMouse>
--- src/components/WorkHistory/WorkHistory.jsx
+++ src/components/WorkHistory/WorkHistory.jsx
@@ -1,16 +1,38 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useLayoutEffect } from 'react';
 import './WorkHistory.scss'
 import workhistorybg from '../../assets/workhistorybg.png';
-import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 const WorkHistory = () => {
-    const containerRef = useRef();
+    const containerRef = useRef(null);
+    const bgImgRef = useRef(null);
+
+    useLayoutEffect(() => {
+        const container = containerRef.current;
+        const bgImg = bgImgRef.current;
+
+        ScrollTrigger.refresh();
+
+        const scrollTrigger = ScrollTrigger.create({
+            trigger: container,
+            start: 'top top',
+            end: () => `${container.offsetHeight - window.innerHeight}px`,
+            pin: bgImg,
+            pinSpacing: true,
+            scrub: 1,
+        });
+
+        return () => {
+            scrollTrigger.kill();
+        };
+    }, []);
 
     return (
         <div ref={containerRef} className="work-history-container">
-            <img className="work-history-bg sticky-bg-img" src={workhistorybg} alt="Background of orange geometric trees with a blue sunrise." />
+            <img ref={bgImgRef} className="work-history-bg-img" src={workhistorybg} alt="Background image of ancient rome building interior with many plants. Watercolor painting." />
 
-            <div className="temp">
+            <div className="container">
+                <p>This is the start of work history temp!!!!!!!!!</p>
                 <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae, ex? Iusto a cupiditate modi minus culpa, quis eos ullam! Vero doloribus non sapiente sit enim quas ipsa, velit id. Soluta?</p>
                 <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae, ex? Iusto a cupiditate modi minus culpa, quis eos ullam! Vero doloribus non sapiente sit enim quas ipsa, velit id. Soluta?</p>
                 <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae, ex? Iusto a cupiditate modi minus culpa, quis eos ullam! Vero doloribus non sapiente sit enim quas ipsa, velit id. Soluta?</p>
