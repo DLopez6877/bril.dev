5fc1ced9d787ea7c5004986ec665c3c9702761c0
DLopez6877 <dlopez6877@gmail.com>
2024-10-20 08:03:35 -0700

    mail to on email button

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -3,6 +3,10 @@ import './Contact.scss';
 import GradientCanvas from '../GradientCanvas/GradientCanvas';
 
 const Contact = () => {
+    const handleEmailClick = () => {
+        window.location.href = 'mailto:dlopez6877@gmail.com?subject=Contact&body=Hello';
+    };
+
     return (
         <div id="contact" className="contact-container">
             <div className="visible-content-container">
@@ -27,7 +31,7 @@ const Contact = () => {
                 </div>
 
                 <footer>
-                    <button aria-label="Send an email">EMAIL ME <i className="fas fa-arrow-right" /></button>
+                    <button onClick={handleEmailClick} aria-label="Send an email to dlopez6877@gmail.com">EMAIL ME <i className="fas fa-arrow-right" /></button>
 
                     <a href="https://www.linkedin.com/in/dlopez6877" target="_blank" rel="noopener noreferrer" aria-label="LinkedIn Profile">
                         <i className="fab fa-linkedin" aria-hidden="true" />
