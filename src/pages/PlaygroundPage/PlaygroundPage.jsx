533883d99456ec12ea0b826ce99660d17039db6a
DLopez6877 <dlopez6877@gmail.com>
2024-10-23 20:47:35 -0700

    add card images

--- src/pages/PlaygroundPage/PlaygroundPage.jsx
+++ src/pages/PlaygroundPage/PlaygroundPage.jsx
@@ -1,83 +1,69 @@
-import React, { useEffect } from 'react';
-import './PlaygroundPage.scss';
-import { Link } from 'react-router-dom';
-import PageTransition from '../../wrappers/PageTransition/PageTransition';
-import SmoothScroll from '../../wrappers/SmoothScroll';
-import SceneLodge from '../../components/Scenes/SceneLodge';
+import React, {useEffect} from "react";
+import "./PlaygroundPage.scss";
+import {Link} from "react-router-dom";
+import PageTransition from "../../wrappers/PageTransition/PageTransition";
+import SmoothScroll from "../../wrappers/SmoothScroll";
+import MinecraftWorld from "../../components/MinecraftWorld/MinecraftWorld";
+import {villageToTree, bridge, train, bookshelf, fullview} from "../../lib/MinecraftScenes";
+
+const PlayPen = () => {
+  return (
+    <>
+      {/* <h2>FOREST</h2>
+      <div className="three-container">
+        <MinecraftWorld cameraPositions={villageToTree} />
+      </div> */}
+
+      {/* <h2>LODGE</h2>
+      <div className="three-container">
+        <MinecraftWorld cameraPositions={bookshelf} />
+      </div> */}
+
+      <h2>FULL</h2>
+      <div className="three-container">
+        <MinecraftWorld cameraPositions={fullview} />
+      </div>
+
+      <h2>ORBIT CONTROLS</h2>
+      <div className="three-container">
+        <MinecraftWorld />
+      </div>
+
+      {/* <h2>TRAIN</h2>
+      <div className="three-container">
+        <MinecraftWorld cameraPositions={train} />
+      </div> */}
+
+      {/* <h2>BRIDGE</h2>
+      <div className="three-container">
+        <MinecraftWorld cameraPositions={bridge} />
+      </div> */}
+    </>
+  );
+};
 
 const PlaygroundPage = () => {
-    useEffect(() => {
-        console.log('PlaygroundPage mounted');
-        return () => {
-            console.log('PlaygroundPage unmounted');
-        };
-    },
-        []);
+  useEffect(() => {
+    console.log("PlaygroundPage mounted");
+    return () => {
+      console.log("PlaygroundPage unmounted");
+    };
+  }, []);
 
-    return (
-        <>
-            {/* <PageTransition> */}
-            <SmoothScroll>
-                <Link className="playground-link" to="/">H</Link>
-                <div className="playground-container" data-scroll-container>
-
-
-
-
-                    {/* ▂▃▅▇█▓▒░    TEMP CODE BELOW HERE    ░▒▓█▇▅▃▂ */}
-                    <div className="three-container">
-                        <SceneLodge />
-                    </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-                </div>
-                {/* END OF TEMP AREA  ¦̵̱ ̵̱ ̵̱ ̵̱ ̵̱(̢ ̡͇̅└͇̅┘͇̅ (▤8כ−◦ */}
-            </SmoothScroll>
-            {/* </PageTransition> */}
-        </>
-
-    );
-
-
-
-
-
-
+  return (
+    <>
+      {/* <PageTransition> */}
+      <SmoothScroll>
+        <Link className="playground-link" to="/">
+          H
+        </Link>
+        <div className="playground-container" data-scroll-container>
+          <PlayPen />
+        </div>
+      </SmoothScroll>
+      {/* </PageTransition> */}
+    </>
+  );
 };
 
 export default PlaygroundPage;
